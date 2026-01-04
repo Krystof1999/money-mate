8049af8a17cee1b3b0b54b15da049b63b60230a2
Krystof Bumbalek <krystofbumbalek2@gmail.com>
2026-01-04 16:44:22 +0100

    cors update to use FE prod

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // local FE
+      "https://money-mate-frontend.netlify.app" // prod FE
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
+
 app.use(express.json());
 
 app.get("/health", (_req: Request, res: Response) => {
